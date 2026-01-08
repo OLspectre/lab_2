55da67f39eda6fda970f5a2c573b2677e43079fc
OLspectre <ossi9910@gmail.com>
2026-01-08 23:49:49 +0100

    users and tasks manually added for later testing

--- src/database/create.sql
+++ src/database/create.sql
@@ -16,3 +16,15 @@ CREATE TABLE tasks (
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+INSERT INTO users (username, password) VALUES
+("Yoman1964", "example1"),
+("BurntPeanut", "example2"),
+("Patapim77", "example2");
+
+INSERT INTO tasks (title, description, status, user_id) VALUES
+("The SQL Initiation", "Write a query to fetch your first user from the shadows of the database.", "completed", 1),
+("Sanitize the Input", "Implement a validation layer to prevent the 'Little Bobby Tables' disaster.", "pending", 1),
+("Async Awakening", "Refactor a blocking function to embrace the non-blocking power of Async/Await.", "completed", 3),
+("The CRUD Ritual", "Complete the cycle of creation, reading, updating, and ultimate destruction.", "pending", 3),
+("Complete the cycle of creation, reading, updating, and ultimate destruction.", "Forge a secure token to allow passage through the protected API gates.", "pending", 3);
