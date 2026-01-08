43901744efdd5c785222ba6ffbcce95cd3c0caea
OLspectre <ossi9910@gmail.com>
2026-01-09 00:57:02 +0100

    basic configuration of server, app.js and database
    Server starts without a problem. Decided on two routes for the project as Seperation of Concerns,

---
+++ app.js
@@ -0,0 +1,22 @@
+import { app } from "./express.js";
+import { db } from "./src/database/connection.js";
+const PORT = process.env.PORT || 3000;
+
+
+(async function startServer() {
+
+    try {
+        const [result] = await db.query("SELECT 'connected' AS status"); // SIMULATED TEST to se if database is connected correctly.
+
+        console.log("db response:", result);
+        console.log("Database connection was successful!");
+        app.listen(PORT, (err) => {
+            (err)
+                ? console.error("Server failed to start", err)
+                : console.log(`Server running on port ${PORT}`);
+        });
+    }
+    catch (err) {
+        console.error("Database connection was NOT successful", err.message);
+    }
+})();
