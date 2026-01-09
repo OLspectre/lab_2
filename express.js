97743dd4d662e36999644dd65b89e8d34b3a55f7
OLspectre <ossi9910@gmail.com>
2026-01-09 01:27:10 +0100

    function to create a new task - ready for testing soon

--- express.js
+++ express.js
@@ -1,14 +1,14 @@
 
 import express from "express";
-import { router as userRouter } from "./src/routes/users.js";
-import { router as tasksRouter } from "./src/routes/tasks.js";
+import { router as userHandlers } from "./src/routes/users.js";
+import { router as taskHandlers } from "./src/routes/tasks.js";
 import { config } from "dotenv";
 
 config();
 const app = express();
 
 app.use(express.json()); //Needed for request body || undefined
-app.use("/users", userRouter);
-app.use("/tasks", tasksRouter);
+app.use("/users", userHandlers);
+app.use("/tasks", taskHandlers);
 
 export { app };
