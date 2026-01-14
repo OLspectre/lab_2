e085ba173ae3076f45333ca1c69ef25b1d1c1f90
OLspectre <ossi9910@gmail.com>
2026-01-14 19:48:16 +0100

    Started set up of login route and for a restricted route needing authorization

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,3 +1,19 @@
 import express from "express";
-
+import { validateTask, validateUserId, validateParamId } from "../middleswares/validation.js";
+import { createTask, getAllTasks, getTaskById, updateTask, deleteTask } from "../models/taskModel.js";
 export const router = express.Router();
+
+
+// /login default route
+router.post("/login", async (req, res, next) => {
+    console.log("login route");
+
+    res.json({ mesasge: "Trying to login via this route" })
+});
+
+router.post("/authorized", async (req, res, next) => {
+    console.log("protected route");
+
+    res.json({ mesasge: "You entered restricted area" })
+});
+
