76d56dc1d67eaa23162496b79906f82f1d032b4f
OLspectre <ossi9910@gmail.com>
2026-01-14 20:10:04 +0100

    first tries of login and creating JWT token

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,18 +2,55 @@ import express from "express";
 import { validateTask, validateUserId, validateParamId } from "../middleswares/validation.js";
 import { createTask, getAllTasks, getTaskById, updateTask, deleteTask } from "../models/taskModel.js";
 export const router = express.Router();
+import jwt from "jsonwebtoken";
 
 
 // /login default route
 router.post("/login", async (req, res, next) => {
     console.log("login route");
+    const username = req.body.username;
+    const password = req.body.password;
 
-    res.json({ mesasge: "Trying to login via this route" })
+    let jwtToken;
+    try {
+        jwtToken = login(username, password);
+
+    } catch (error) {
+        next(error);
+    }
+
+    res.json({
+        type: "success",
+        mesasge: "User was authenticated correctly",
+        token: jwtToken,
+        payload: jwt.decode(jwtToken)
+    })
 });
 
-router.post("/authorized", async (req, res, next) => {
-    console.log("protected route");
 
-    res.json({ mesasge: "You entered restricted area" })
-});
+function login(username, password) {
+    if (username !== "admin" || password !== "password123") {
+        throw new Error("User and password does not match");
+    }
+
+    const payload = {
+        iss: "Issue id",
+        sub: username,
+        iat: Date.now,
+        username,
+    }
+
+    const options = {
+        expiresIn: "15m"
+    }
+
+    const token = jwt.sign(payload, "SPECIAL KEY", options);
+    return token;
+};
+
+// router.get("/authorized", async (req, res, next) => {
+//     console.log("protected route");
+
+//     res.json({ mesasge: "You entered restricted area" })
+// });
 
